057c6e5c80c4640489a6eb7bf1e93ebe4ea0101a
FernandesFilipe <Filipegoulartfernandes@gmail.com>
2024-08-05 15:57:08 +0100

    Mostrar mensagem "Preencha os campos"

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +24,27 @@ class MainActivity : AppCompatActivity() {
 
         btncalcular.setOnClickListener {
 
-            val  peso: Float = edtpeso.text.toString().toFloat()
-            val altura: Float = edtaltura.text.toString().toFloat()
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-            println("acaodoroque" + resultado)
+            val  pesoStr: String = edtpeso.text.toString()
+            val alturaStr: String = edtaltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == ""){
+                //MOSTRAR MENSAGEM PARA O USUARIO
+                Snackbar
+                    .make (
+                        edtpeso,
+                        "Preencha todos os campos" ,
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+            }
+            else {
+
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+                println("acaodoroque" + resultado)
+            }
         }
     }
 }
